a94a0c09dadae1b30db604d5ee88e5ef743a092c
ChayanonLivingOS <chayanon@thelivingos.com>
2022-03-06 23:56:08 +0700

    fix file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 ////require
-const router = require(`..salary-node-ying/router/myRouter`);
+//const router = require(`..salary-node-ying/router/myRouter`);
 const express = require(`express`);
 const path = require(`path`);
 const app = express();
@@ -15,8 +15,47 @@ next();
 app.set('views', path.join(__dirname ,'views'));
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:false}));
-app.use(router);
+//app.use(router);
 app.use(express.static(path.join(__dirname,'public')));
+
+app.get(`/`, (req, res) => {
+    let data = {
+        salary: "",
+        pt: "",
+        ot:"",
+        sum: "",
+    };
+    res.render('index' ,{cal:data});
+});
+
+app.post('/cal',(req,res) =>{
+    let costpt = 166;
+    let resSala = Math.floor(req.body.salary);
+
+    if(req.body.pt !== 0){
+        resSala = (costpt * req.body.pt)
+    }
+
+    if(req.body.ot !== 0){
+        let day = 30;
+        let workhours = 11;
+        let plus = 1.5;
+
+        let calOt = (((req.body.salary / day / workhours) * plus) * req.body.ot);
+        let tempPt = costpt * req.body.pt;
+        resSala = (Math.floor(req.body.salary) + tempPt + calOt).toFixed(2);
+    }
+    let data = {
+        salary:req.body.salary,
+        pt:req.body.pt,
+        ot:req.body.ot,
+        sum:resSala
+    }
+    res.render(`index`,{cal:data});
+})
+
+
+
 app.listen(PORT , ()=>{
     console.log(`Run server at port 5000`);
 });
