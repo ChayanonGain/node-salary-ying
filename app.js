dffe24043507d4d47f1a6f4bb14244be6aca1430
ChayanonLivingOS <chayanon@thelivingos.com>
2022-03-06 18:17:22 +0700

    edit file

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const express = require(`express`);
 const path = require(`path`);
 const app = express();
 
+let PORT = process.env.PORT || 5007
+
 
 app.set('views', path.join(__dirname ,'views'));
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:false}));
 app.use(router);
 app.use(express.static(path.join(__dirname,'public')));
-app.listen(5007 , ()=>{
+app.listen(PORT , ()=>{
     console.log(`Run server at port 5007`);
 });
